fe61bcb7ad4b3fccdade5fbec4f0ea4c52433dcd
Shubh Bapna <shubh.bapna@mail.utoronto.ca>
2023-11-17 01:05:12 -0500

    warm up addresses

--- src/reverse.cc
+++ src/reverse.cc
@@ -10,15 +10,19 @@ int main(int argc, char **argv) {
     const long int num_iterations = BUFFER_SIZE_MB / ROW_SIZE;
     char *base = (char *)allocated_mem;
     uint64_t same_row_lat = 0;
+    // warm the address up
+    measure_bank_latency((uint64_t)base, (uint64_t)(base));
     for (int k = 0; k < SAMPLES; k++) {
         same_row_lat += measure_bank_latency((uint64_t)base, (uint64_t)(base));
     }
     same_row_lat = same_row_lat / SAMPLES;
-    printf("Same row latency: %ld", same_row_lat);
+    printf("Same row latency: %ld\n", same_row_lat);
 
     int index_y;
     for (int i = 1; i < num_iterations; i++) {
         uint64_t time = 0;
+        // warm the address up to avoid page faults and TLB misses
+        measure_bank_latency((uint64_t)base, (uint64_t)(base + i * ROW_SIZE));
         for (int k = 0; k < SAMPLES; k++) {
             time += measure_bank_latency((uint64_t)base, (uint64_t)(base + i * ROW_SIZE));
         }
@@ -32,7 +36,7 @@ int main(int argc, char **argv) {
     uint64_t x = virt_to_phys((uint64_t) base);
     uint64_t y = virt_to_phys((uint64_t) (base + index_y * ROW_SIZE));
     printf("X: %ld (phys)\t%ld (virt)\n", x, (uint64_t) base);
-    printf("Y: %ld (phys)\t%ld (virt)\n", y, (uint64_t) (base + index_y * ROW_SIZE));
+    printf("Y: %ld (phys)\t%d (virt)\n", y, index_y);
 
     int index_a[5];
     int count = 0;
@@ -40,6 +44,7 @@ int main(int argc, char **argv) {
         if (i == index_y) continue;
         
         uint64_t time = 0;
+        measure_bank_latency((uint64_t)base, (uint64_t)(base + i * ROW_SIZE));
         for (int k = 0; k < SAMPLES; k++) {
             time += measure_bank_latency((uint64_t)base, (uint64_t)(base + i * ROW_SIZE));
         }
@@ -47,6 +52,7 @@ int main(int argc, char **argv) {
         
         if (time < ROW_BUFFER_HIT_LATENCY) {
             time = 0;
+            measure_bank_latency((uint64_t)(base + index_y * ROW_SIZE), (uint64_t)(base + i * ROW_SIZE));
             for (int k = 0; k < SAMPLES; k++) {
                 time += measure_bank_latency((uint64_t)(base + index_y * ROW_SIZE), (uint64_t)(base + i * ROW_SIZE));
             }
@@ -61,6 +67,6 @@ int main(int argc, char **argv) {
 
     for (int i = 0; i < count; i++) {
         uint64_t a = virt_to_phys((uint64_t) (base + index_a[i] * ROW_SIZE));
-        printf("A: %ld(phy)\t%ld\n", a, (uint64_t) (base + index_a[i] * ROW_SIZE));
+        printf("A: %ld (phy)\t%d (virt)\n", a, index_a[i]);
     }
 }
