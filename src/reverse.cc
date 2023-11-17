7ac0ebca8e32d05c096d137e98e3a1962ab40bf8
Shubh Bapna <shubh.bapna@mail.utoronto.ca>
2023-11-16 20:08:46 -0500

    fix latency bounds

--- src/reverse.cc
+++ src/reverse.cc
@@ -10,41 +10,46 @@ int main(int argc, char **argv) {
     const long int num_iterations = BUFFER_SIZE_MB / ROW_SIZE;
     char *base = (char *)allocated_mem;
 
-    int i;
-    for (i = 1; i < num_iterations; i++) {
+    int index_y;
+    for (int i = 1; i < num_iterations; i++) {
         uint64_t time = 0;
         for (int k = 0; k < SAMPLES; k++) {
             time += measure_bank_latency((uint64_t)base, (uint64_t)(base + i * ROW_SIZE));
         }
         time = time / SAMPLES;
-        if (ROW_BUFFER_CONFLICT_LATENCY_MIN < time < ROW_BUFFER_CONFLICT_LATENCY_MAX) {
+        if (ROW_BUFFER_CONFLICT_LATENCY_MIN < time) {
+            index_y = i;
             break;
         }
     }
+
     uint64_t x = virt_to_phys((uint64_t) base);
-    uint64_t y = virt_to_phys((uint64_t) (base + i * ROW_SIZE));
+    uint64_t y = virt_to_phys((uint64_t) (base + index_y * ROW_SIZE));
     printf("X: %ld, Y: %ld, ", x, y);
 
-    int j;
-    for (j = 1; j < num_iterations; j++) {
-        if (j == i) continue;
+    int index_a;
+    for (int i = 1; i < num_iterations; i++) {
+        if (i == index_y) continue;
+        
         uint64_t time = 0;
         for (int k = 0; k < SAMPLES; k++) {
-            time += measure_bank_latency((uint64_t)base, (uint64_t)(base + j * ROW_SIZE));
+            time += measure_bank_latency((uint64_t)base, (uint64_t)(base + i * ROW_SIZE));
         }
         time = time / SAMPLES;
+        
         if (time < ROW_BUFFER_HIT_LATENCY) {
             time = 0;
             for (int k = 0; k < SAMPLES; k++) {
-                time += measure_bank_latency((uint64_t)(base + i * ROW_SIZE), (uint64_t)(base + j * ROW_SIZE));
+                time += measure_bank_latency((uint64_t)(base + index_y * ROW_SIZE), (uint64_t)(base + i * ROW_SIZE));
             }
             time = time / SAMPLES;
-            if (ROW_BUFFER_CONFLICT_LATENCY_MIN < time < ROW_BUFFER_CONFLICT_LATENCY_MAX) {
+            if (ROW_BUFFER_CONFLICT_LATENCY_MIN < time) {
+                index_a = i;
                 break;
             }    
         }
     }
 
-    uint64_t a = virt_to_phys((uint64_t) (base + j * ROW_SIZE));
+    uint64_t a = virt_to_phys((uint64_t) (base + index_a * ROW_SIZE));
     printf("A: %ld\n", a);
 }
