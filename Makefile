e88f8592dfa0dcd248f21350e0f08dd013aad476
Shubh Bapna <shubh.bapna@mail.utoronto.ca>
2024-04-03 18:38:23 -0400

    run press all and find multiple rows in one run

--- Makefile
+++ Makefile
@@ -1,31 +1,32 @@
 CC=g++
 FLAGS=-std=c++11 -g -O0
 ADDRESS_SANITIZER=-fsanitize=address
+CFLAGS=-DVERBOSE=0
 
 all: bin/histogram bin/conflicting-addresses bin/detect-col-bits bin/verify-dram-mapping bin/hammer bin/time-act bin/press | bin
 clean:
 	rm -rf bin/
 
 bin/histogram: src/histogram/histogram.cc src/shared.cc src/shared.hh src/params.hh src/util.hh | bin
-	$(CC) $(FLAGS) -o $@ src/histogram/histogram.cc src/util.hh src/shared.cc
+	$(CC) $(CFLAGS) $(FLAGS) -o $@ src/histogram/histogram.cc src/util.hh src/shared.cc
 
 bin/conflicting-addresses: src/reverse/conflicting-addresses.cc src/shared.cc src/shared.hh src/params.hh src/util.hh | bin
-	$(CC) $(FLAGS) -o $@ src/reverse/conflicting-addresses.cc src/util.hh src/shared.cc
+	$(CC) $(CFLAGS) $(FLAGS) -o $@ src/reverse/conflicting-addresses.cc src/util.hh src/shared.cc
 
 bin/verify-dram-mapping: src/reverse/verify-dram-mapping.cc src/shared.cc src/shared.hh src/params.hh src/util.hh | bin
-	$(CC) $(FLAGS) -o $@ src/reverse/verify-dram-mapping.cc src/util.hh src/shared.cc
+	$(CC) $(CFLAGS) $(FLAGS) -o $@ src/reverse/verify-dram-mapping.cc src/util.hh src/shared.cc
 
 bin/detect-col-bits: src/reverse/detect-col-bits.cc src/shared.cc src/shared.hh src/params.hh src/util.hh | bin
-	$(CC) $(FLAGS) -o $@ src/reverse/detect-col-bits.cc src/util.hh src/shared.cc
+	$(CC) $(CFLAGS) $(FLAGS) -o $@ src/reverse/detect-col-bits.cc src/util.hh src/shared.cc
 
 bin/hammer: src/hammer/hammer.cc src/shared.cc src/shared.hh src/params.hh src/util.hh | bin
-	$(CC) $(FLAGS) $(ADDRESS_SANITIZER) -o $@ src/hammer/hammer.cc src/util.hh src/shared.cc
+	$(CC) $(CFLAGS) $(FLAGS) $(ADDRESS_SANITIZER) -o $@ src/hammer/hammer.cc src/util.hh src/shared.cc
 
 bin/time-act: src/hammer/time-act.cc src/shared.cc src/shared.hh src/params.hh src/util.hh | bin
-	$(CC) $(FLAGS) -o $@ src/hammer/time-act.cc src/util.hh src/shared.cc
+	$(CC) $(CFLAGS) $(FLAGS) -o $@ src/hammer/time-act.cc src/util.hh src/shared.cc
 
 bin/press: src/press/press.cc src/shared.cc src/shared.hh src/params.hh src/util.hh | bin
-	$(CC) $(FLAGS) $(ADDRESS_SANITIZER) -o $@ src/press/press.cc src/util.hh src/shared.cc
+	$(CC) $(CFLAGS) $(FLAGS) $(ADDRESS_SANITIZER) -o $@ src/press/press.cc src/util.hh src/shared.cc
 
 bin:
 	mkdir bin
--- src/press/press.cc
+++ src/press/press.cc
@@ -4,6 +4,14 @@
 #include <unistd.h>
 #include <set>
 
+#ifndef VERBOSE
+    #define VERBOSE 0
+#endif
+
+uint64_t mem_size = 1.8 * BUFFER_SIZE;
+FILE* verified_results;
+FILE* non_verified_results;
+
 uint32_t press(uint64_t vict_virt_addr, uint64_t attacker_virt_addr_1, uint64_t attacker_virt_addr_2) {
     // prime
     uint8_t *vict_virt_addr_ptr = reinterpret_cast<uint8_t *>(vict_virt_addr);
@@ -38,49 +46,47 @@ uint32_t press(uint64_t vict_virt_addr, uint64_t attacker_virt_addr_1, uint64_t 
     flush_row(vict_virt_addr_ptr);
 
     uint32_t number_of_bitflips_in_target = count_flips(vict_virt_addr_ptr, 0x55);
-    print_result(vict_virt_addr, attacker_virt_addr_1, attacker_virt_addr_2, number_of_bitflips_in_target);
-    if (number_of_bitflips_in_target) {
+    if (VERBOSE > 0) {
+        print_result(vict_virt_addr, attacker_virt_addr_1, attacker_virt_addr_2, number_of_bitflips_in_target);
+    }
+    if (VERBOSE > 1 && number_of_bitflips_in_target) {
         print_diff(vict_virt_addr_ptr, 0x55);
     }
     return number_of_bitflips_in_target; 
 }
 
 void press_all() {
-    uint64_t mem_size = 1.8 * BUFFER_SIZE;
-    allocated_mem = allocate_pages(mem_size);
-    setup_PPN_VPN_map(allocated_mem, mem_size);
-
     uint64_t victim; 
     uint64_t* attacker_1 = (uint64_t*) calloc(1, sizeof(uint64_t));
     uint64_t* attacker_2 = (uint64_t*) calloc(1, sizeof(uint64_t)); 
-
+    uint32_t num_bit_flips;
     while (true) {
         victim = (uint64_t)((uint8_t *)allocated_mem + ROW_SIZE * (rand() % (mem_size / PAGE_SIZE)));
         // row + 1, row - 1
         if (get_addresses_to_hammer(virt_to_phys(victim), attacker_1, attacker_2, 1)) {
-            uint32_t num_bit_flips = press(victim, *attacker_1, *attacker_2);
+            num_bit_flips = press(victim, *attacker_1, *attacker_2);
             if (num_bit_flips > 0) {
-                printf("Found vulnerable row. Hammering again...\n");
-                num_bit_flips = press(victim, *attacker_1, *attacker_2);
+                break;
             }
         }
     }
-    deallocate_pages(allocated_mem, mem_size);
+    printf("Found vulnerable row at %ld\n", victim);
+    num_bit_flips = press(victim, *attacker_1, *attacker_2);
+    FILE *fp = non_verified_results;
+    if (num_bit_flips > 0) {
+        fp = verified_results;
+    }
+    fwrite(&victim, sizeof(uint64_t), 1, fp);
+    fwrite("\n", 1, 1, fp);
     free(attacker_1);
     free(attacker_2);
 }
 
 void press_one(uint64_t *victims, int num_victims) {
-    uint64_t mem_size = 1.8 * BUFFER_SIZE;
-    uint64_t* victims_virt_addr = (uint64_t*) calloc(num_victims, sizeof(uint64_t));
     std::set<payload *> payloads;
 
     while (true) {
         printf("Attempting to locate physical addresses\n");
-        void *temp = allocate_pages(mem_size);
-        deallocate_pages(allocated_mem, mem_size);
-        allocated_mem = temp;
-        setup_PPN_VPN_map(allocated_mem, mem_size);
 
         int i;
         for (i = 0; i < num_victims; i++) {
@@ -98,6 +104,10 @@ void press_one(uint64_t *victims, int num_victims) {
             printf("Located all address. Will start pressing now.\n");
             break;
         }
+        void *temp = allocate_pages(mem_size);
+        deallocate_pages(allocated_mem, mem_size);
+        allocated_mem = temp;
+        setup_PPN_VPN_map(allocated_mem, mem_size);
     }
 
     uint64_t rand_victim; 
@@ -128,15 +138,21 @@ void press_one(uint64_t *victims, int num_victims) {
         print_result(f->victim, f->attacker1, f->attacker2, f->bit_flips);
         free(f);
     }
-    deallocate_pages(allocated_mem, mem_size);
     free(rand_attacker_1);
     free(rand_attacker_2);
 }
 
 int main(int argc, char **argv) {
     setvbuf(stdout, NULL, _IONBF, 0);
+    allocated_mem = allocate_pages(mem_size);
+    setup_PPN_VPN_map(allocated_mem, mem_size);
+    verified_results = fopen("verified.txt", "a");
+    non_verified_results = fopen("non_verified.txt", "a");
     if (argc == 1) {
-        press_all();
+        while (true) {
+            // not changing the if statement inside the function as it causes some weird branch prediction
+            press_all();
+        }
     } else {
         uint64_t victims[argc - 1];
         for (int i = 1; i < argc; i++) {
@@ -144,5 +160,6 @@ int main(int argc, char **argv) {
         }
         press_one(victims, argc - 1);
     }
+    deallocate_pages(allocated_mem, mem_size);
 }
 
